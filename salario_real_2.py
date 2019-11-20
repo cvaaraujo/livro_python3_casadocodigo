15ef120d306d06cbbfd3ff58f2bca036faba2ffe
Vinicius Araujo <vinny.engenharia@gmail.com>
2019-11-19 22:46:53 -0300

    segundo programa do Livro Python3 - Casa do Código

---
+++ salario_real_2.py
@@ -0,0 +1,7 @@
+salario = int(input('Salario: '))
+imposto = input('Imposto em % (ex: 27.5): ')
+if imposto == '':
+    imposto = 27.5
+else:
+    imposto = float(imposto)
+print("Valor real: {0}".format(salario * (imposto * 0.01)))
